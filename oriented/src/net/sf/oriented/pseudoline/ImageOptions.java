593cc1c776af6e726b432c8895ec018419320034
Jeremy Carroll <jjc@syapse.com>
2013-08-25 09:12:51 -0700

    options looking fairly complete (for color case)

--- oriented/src/net/sf/oriented/pseudoline/ImageOptions.java
+++ oriented/src/net/sf/oriented/pseudoline/ImageOptions.java
@@ -4,6 +4,11 @@
 package net.sf.oriented.pseudoline;
 
 import java.awt.Color;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 import net.sf.oriented.omi.Label;
 
 public class ImageOptions {
@@ -29,26 +34,64 @@ public class ImageOptions {
     public double originArrowSize;
 
     private double internalFontSizeRatio = 5;
+    
+    private Color infinityColor;
+    private double infinityStroke[];
+    
+    private Map<Label,Color> colors = new HashMap<Label,Color>();
+    private Map<Label,double[]> strokes = new HashMap<Label,double[]>();
+    private Label infinity;
+    private int nextColor = 0;
+    private int nextStroke = 0;
+    
+    private static Color someColors[] = new Color[22];
 
+    /**
+     * Can be null
+     * @param c
+     */
     public void setColorOfInfinity(Color c) {
-        
+        infinityColor = c;
     }
+    /**
+     * Can be null
+     * @param dashes
+     */
     public void setStrokeOfInfinity(double ... dashes) {
-        
+        infinityStroke = dashes;
     }
     public void setColor(Label lbl, Color c) {
+        colors.put(lbl,c);
         
     }
     public void setStroke(Label lbl, double ...dashes ) {
-        
+        strokes.put(lbl,dashes);
     }
 
     public static ImageOptions defaultBlackAndWhite() {
         return null;
     }
-    
-    Color someColors[];
-    double someDashes[][] = new double[][]{
+    static {
+        int ix = 0;
+        for (int i=0;i<3;i++)
+            for (int j=0;j<3;j++)
+                for (int k=0;k<3;k++) {
+                    if (i==j && j==k)
+                        continue;
+                    if (i==2 && j==2 && k == 1) // too close to our background color
+                        continue;
+                    if (i==2 && j==1 && k == 2) // too close to our background color
+                        continue;
+                    someColors[ix++] = new Color((3+6*i)*17,(3+6*j)*17,(3+6*k)*17);
+                }
+        Arrays.sort(someColors,new Comparator<Color>(){
+            @Override
+            public int compare(Color o1, Color o2) {
+                // pseudorandom order
+                return System.identityHashCode(o1) - System.identityHashCode(o2);
+            }});
+    }
+    private static double someStrokes[][] = new double[][]{
             { 10, 3 },
             { 3, 3 },
             { 5, 3, 2, 3, 2, 3 },
@@ -80,12 +123,31 @@ public class ImageOptions {
         return rslt;
     }
     
-    private Label infinity;
+    double fontSizeRatio() {
+        return this.fontSizeRatio * this.internalFontSizeRatio;
+    }
+    
     void setInfinity(Label inf) {
         infinity = inf;
     }
-    Color getColor(Label lbl) { return null; }
-    double[] getStroke(Label lbl){ return null; }
+    Color getColor(Label lbl) { 
+        if ( lbl == infinity && infinityColor != null) {
+            return infinityColor;
+        }
+        if (colors.get(lbl)==null) {
+            colors.put(lbl, someColors[nextColor++]);
+        }
+        return colors.get(lbl);
+    }
+    double[] getStroke(Label lbl){ 
+        if ( lbl == infinity && infinityStroke != null) {
+            return infinityStroke;
+        }
+        if (strokes.get(lbl)==null) {
+            strokes.put(lbl, someStrokes[nextStroke++]);
+        }
+        return strokes.get(lbl);
+    }
     
 }
 
