e051d6fcf94a1641eff4a528b037b4bc819ee4d3
Jeremy Carroll <jjc@syapse.com>
2013-09-02 10:13:08 -0700

    javadoc enhancements

--- oriented/src/oriented/Image.java
+++ oriented/src/oriented/Image.java
@@ -24,6 +24,86 @@ import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.MutuallyExclusiveGroup;
 import net.sourceforge.argparse4j.inf.Namespace;
 
+/**
+ * 
+Create a pseudoline picture corresponding to one of the example oriented matroids.
+
+Usage:
+<pre>
+Image [-h] [-W WIDTH] [-L LINE-WIDTH] [-H HEIGHT] [-o OUTPUT] [-m] [--nolabels] [--novertex]
+             [--noorigin] [-∞ INFINITY] (--wheel12 | --tsukamoto13.-1 | --chapter1 | --πάππος |
+             --omega14.-1 | --Ω14.0 | --omega14.0 | --circularsaw3 | --suvorov14 | --tsukamoto13.+1 |
+             --ceva | --tsukamoto13.0 | --pappus | --Ω14.+1 | --omega14.+1 | --Ω14.-1 | --uniform4 |
+             --uniform3 | --ringel) [--png | --jpeg | --x-png | --vnd.wap.wbmp | --gif | --bmp]
+</pre>
+<p>
+optional arguments:
+</p>
+<dl>
+<dt>
+  -h, --help
+<dt><dd>show this help message and exit</dd>
+<dt>-W WIDTH, --width WIDTH</dt><dd>
+                         set the width of the output.</dd>
+<dt>-L LINE-WIDTH, --line-width LINE-WIDTH</dt><dd>
+                         set the width of the lines.</dd>
+<dt>-H HEIGHT, --height HEIGHT</dt><dd>
+                         set the height of the output.</dd>
+<dt>-o OUTPUT, --output OUTPUT</dt><dd>
+                         sets the output file.</dd>
+<dt>-m, --monochrome</dt><dd>       black and white.</dd>
+<dt>--nolabels</dt><dd>             omit labels.</dd>
+<dt>--novertex</dt><dd>              omit vertex indicators.</dd>
+<dt>--noorigin</dt><dd>              omit origin marker.</dd>
+<dt>-∞ INFINITY, -8 INFINITY, --infinity INFINITY</dt><dd> 
+                         The label of the line at infinity</dd>
+</dl>
+<p>
+Select exactly one of the following to choose the oriented matroid. The list in this documentation may be out of date,
+use the <code>--help</code> option to get the up-to-date list.
+</p>
+<ul>
+<li>
+  --wheel12 </li><li>
+  --tsukamoto13.-1</li><li>
+  --chapter1             </li><li>
+  --πάππος               </li><li>
+  --omega14.-1           </li><li>
+  --Ω14.0                </li><li>
+  --omega14.0            </li><li>
+  --circularsaw3         </li><li>
+  --suvorov14            </li><li>
+  --tsukamoto13.+1       </li><li>
+  --ceva                 </li><li>
+  --tsukamoto13.0        </li><li>
+  --pappus               </li><li>
+  --Ω14.+1               </li><li>
+  --omega14.+1           </li><li>
+  --Ω14.-1               </li><li>
+  --uniform4             </li><li>
+  --uniform3             </li><li>
+  --ringel               </li>
+</ul>
+<p>
+The image format defaults as jpeg, the following list of alternatives is system dependent.
+The list in this documentation reflects those available on a Mac 10.8.4, with Java 1.7.0.21.
+Use the <code>--help</code> option to see the options available to you.
+</p>
+<dl>
+<dt>
+  --png</dt><dd>                    Output in image/png format.</dd>
+<dt>
+  --jpeg   </dt><dd>                Output in image/jpeg format.</dd>
+<dt>
+  --x-png   </dt><dd>               Output in image/x-png format.</dd>
+<dt>
+  --vnd.wap.wbmp   </dt><dd>        Output in image/vnd.wap.wbmp format.</dd>
+<dt>
+  --gif           </dt><dd>         Output in image/gif format.</dd>
+<dt>
+  --bmp          </dt><dd>          Output in image/bmp format.</dd>
+</dl>
+ */
 public class Image {
     
     private Image(){}
