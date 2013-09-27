65cbf478cca31e490be05c748746320dfe62ff1e
Jeremy Carroll <jjc@syapse.com>
2013-09-27 06:01:12 -0700

    initial clone of WAM etc. does not compile

---
+++ oriented/src/net/sf/oriented/pseudoline2/GrowingGraph.java
@@ -0,0 +1,67 @@
+/************************************************************************
+  (c) Copyright 2013 Jeremy J. Carroll
+ ************************************************************************/
+package net.sf.oriented.pseudoline2;
+
+import java.util.List;
+
+
+/**
+ * This graph is created from a TensionGraph, which should
+ * remain unchanged during the initialization of the twisted graph.
+ * 
+ * The twisted graph is built by adding edges from the TensionGraph until
+ * the {@link #isTwistedGraph()} property returns true.
+ * @author jeremycarroll
+ *
+ */
+public class GrowingGraph extends AbstractTGraph {
+    
+    private final ShrinkingGraph parent;
+    private final WAM wam;
+    public GrowingGraph(WAM wam, ShrinkingGraph parent) {
+       this.parent = parent; 
+       this.wam = wam;
+    }
+
+    
+    /**
+     * 
+     * @param t
+     * @return true if added, false if it cannot be added.
+     */
+    public boolean addWithConsequences(TGEdge t) {
+        rawAdd(t);
+        return parent.removeParallel(t) && parent.consequences(t.source, t, this) && parent.consequences(t.dest, t, this);
+    }
+    
+
+    private void rawAdd(TGEdge t) {
+        if (!addEdge(t, t.source, t.dest)) {
+            throw new IllegalArgumentException("addEdge failed!");
+        }
+        wam.pushUndoRemove(this,t);
+    }
+
+
+    
+    public void addChoices(TGVertex face, List<List<TGEdge>> choices) {
+        EdgeChoices opt = new EdgeChoices(face,choices);
+        wam.addChoice(opt);
+    }
+}
+
+
+/************************************************************************
+    This file is part of the Java Oriented Matroid Library.  
+
+    The Java Oriented Matroid Library is distributed in the hope that it 
+    will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the Java Oriented Matroid Library.  
+    If not, see <http://www.gnu.org/licenses/>.
+
+**************************************************************************/
---
+++ oriented/src/net/sf/oriented/pseudoline2/WAM.java
@@ -0,0 +1,488 @@
+/************************************************************************
+  (c) Copyright 2013 Jeremy J. Carroll
+ ************************************************************************/
+package net.sf.oriented.pseudoline2;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * General back tracking design notes:
+ * 
+ * basic inspriation is the warren machine with four ports;
+ * - call: takes next entry in call stack, records current trail ptr in call stack, does first option of call
+ * - backto: undoes trail
+ * - retry: takes next option of head of call stack
+ * - fail: pop calls stack
+ * with fifth port:
+ * - expand: adds a new call to an empty call stack
+ * 
+ * this class keeps a call stack and a trail.
+ * 
+ * two methods {? and ?} are used for generating new entries for the call stack
+ * 
+ * currently the call stack is a bit naive forward looking, in that either there are no entries
+ * or there is one.
+ * 
+ * if there are no entries then one of the two methods is used to generate the next entry, if 
+ * none is forthcoming then we have failed.
+ * 
+ * after every call or retry we check for success
+ * 
+ * it may be the case that the search finds two twisted graphs where the latter is a proper subgraph of the former, 
+ * this needs to be explicitly checked for
+ * 
+ * each call stack entry has a pointer to the trail before the call
+ * 
+ * when we backtrack to a call stack entry, we undo the trail
+ * 
+ * trail changes include changes to the twisted graph and the parent tension graph
+ * 
+ * 
+ * 
+ * 
+ * @author jeremycarroll
+ *
+ */
+public class WAM {
+    
+    private final class MakeChoiceAtFace extends Frame {
+        private final EdgeChoices opt;
+        private final TGEdge[] singleChoices;
+        private final TGEdge[] doubleChoices;
+
+        private MakeChoiceAtFace(EdgeChoices opt) {
+            this.opt = opt;
+            this.singleChoices = opt.singleChoices();
+            this.doubleChoices = opt.doubleChoices();
+        }
+
+        @Override
+        public String toString() {
+            return opt.face.covector().toString() +":" + this.retryCount + "/" +(singleChoices.length+doubleChoices.length/2)+" "+edgeLabel(retryCount);
+        }
+
+        private String edgeLabel(int ix) {
+        if (ix < singleChoices.length) {
+            return singleChoices[ix].toString();
+        } else {
+            ix -= singleChoices.length;
+            if (ix*2 >= doubleChoices.length) {
+                return "";
+            }
+            return doubleChoices[2*ix].toString()+","+doubleChoices[2*ix+1].toString();
+        }
+         }
+
+        @Override
+        boolean call(int ix) {
+            if (!removeChoice(ix-1)) {
+                fail();
+                return false;
+            }
+            if (ix < singleChoices.length) {
+                return add(singleChoices[ix]);
+            } else {
+                ix -= singleChoices.length;
+                if (ix*2 == doubleChoices.length) {
+                    fail();
+                    return false;
+                }
+                if (!  add(doubleChoices[2*ix])  )
+                    return false;
+                TGEdge t = doubleChoices[2*ix+1];
+                if (growing.containsEdge(t)) 
+                    return true;
+                return add(t);
+            }
+        }
+
+        /**
+         * 
+         * @param ix
+         * @return true if operation omitted or successful, false to force backtracking
+         */
+        private boolean removeChoice(int ix) {
+            if ( ix== -1) {
+                return true;
+            }
+            if (ix < singleChoices.length) {
+                boolean rslt = maybeRemove(singleChoices[ix]);
+                this.trailPtr = trail.size();
+                return rslt;
+            } 
+            return true;
+        }
+    }
+
+    private final class Failure extends Frame {
+        @Override
+        boolean call(int counter) {
+            fail();
+            return false;
+        }
+        @Override
+        public String toString() {
+            return "fail";
+        }
+    }
+    private enum Port {
+        Call, Redo, Fail
+    }
+
+    private abstract class Frame {
+
+        Port port = Port.Call;
+        int trailPtr;
+        int retryCount = 0;
+
+        final boolean call() {
+            trailPtr = trail.size();
+            port = Port.Redo;
+            return retry();
+        }
+
+        final boolean retry() {
+            return call(retryCount++);
+        }
+        
+        abstract boolean call(int counter);
+  
+        protected void fail() {
+            port = Port.Fail;
+        }
+    }
+    
+    private abstract class Undoable {
+        public abstract void undo() ;
+    }
+
+    private final TensionGraph base;
+    /**
+     * The current solution space is the set of graphs between growing and shrinking.
+     * As the algorithm progresses these get closer together until we hit success!
+     */
+    private final GrowingGraph growing;
+    private final ShrinkingGraph shrinking;
+    private final Deque<Frame> stack = new ArrayDeque<Frame>();
+    private final Deque<Undoable> trail = new ArrayDeque<Undoable>();
+    private final List<Difficulty> results = new ArrayList<Difficulty>();
+    final Deque<EdgeChoices> choices = new ArrayDeque<EdgeChoices>();
+    
+    // tracing and debug fields
+    public int transitions = 0;
+    private AbstractTGraph expected;
+    public boolean debug;
+    
+    public WAM(TensionGraph b) {
+        base = b;
+        shrinking = new ShrinkingGraph(this, base);
+        growing = shrinking.growing;
+    }
+    
+    public void computeCycles() {
+        TensionPaths simplePaths = new TensionPaths(this, shrinking);
+        
+    }
+    /**
+     * 
+     * @param tension
+     * @return true if operation omitted or successful, false to force backtracking
+     */
+    public boolean maybeRemove(TGEdge tension) {
+        if (shrinking.containsEdge(tension)) {
+            return remove(tension);
+        }
+        return true;
+    }
+
+    public List<Difficulty> search() {
+        extend();
+        while (true) {
+            Frame top = stack.peek();
+            this.debugMsg(top.port.name());
+            switch (top.port) {
+            case Call:
+                if (top.call()) {
+                    if (success()) {
+                        backTrack();
+                    } else {
+                       extend();
+                    }
+                } else {
+                    backTrack();
+                }
+                break;
+            case Redo:
+                if (top.retry()) {
+                    if (success()) {
+                        backTrack();
+                    } else {
+                       extend();
+                    }
+                } else {
+                    backTrack();
+                }
+                break;
+            case Fail:
+                stack.pop();
+                if (stack.isEmpty())
+                    return results;
+                backTrack();
+                break;
+            }
+        }
+    }
+
+    
+
+    void addChoice(final EdgeChoices opt) {
+        choices.push(opt);
+
+        trail.push(new Undoable(){
+            @Override
+            public void undo() {
+                choices.remove(opt);
+            }});
+    }
+
+
+    public boolean hasOptions() {
+        return !choices.isEmpty();
+    }
+    private boolean success() {
+        boolean success = growing.isTwistedGraph();
+        if (success) {
+            results.add(new Difficulty(growing));
+//            growing.dumpEdges();
+        }
+        return success;
+    }
+
+    private void extend() {
+        if ( hasOptions() ) {
+            pushChoiceFromEdge(selectNextEdgeChoice());
+        } else {
+            final TGEdge t = findPossibleEdge();
+            if (t==null) {
+                stack.push(new Failure());
+            } else {
+            stack.push(new Frame(){
+                @Override
+                boolean call(int ix) {
+                    switch(ix) {
+                    case 0:
+                        return add(t);
+                    case 1:
+                        fail();
+                        return remove(t);
+                    default:
+                        throw new IllegalArgumentException("WAM call logic error");
+                    }
+                }
+                @Override
+                public String toString() {
+                    switch (this.retryCount) {
+                case 0:
+                    return "add "+t.toString();
+                case 1:
+                    return "[-add] "+t.toString();
+                case 2:
+                    return "fail [add] "+t.toString();
+                default:
+                    throw new IllegalArgumentException("WAM call logic error");
+                }
+                }
+            });
+            }
+        }
+    }
+
+    private EdgeChoices selectNextEdgeChoice() {
+        Iterator<EdgeChoices> it = choices.iterator();
+        EdgeChoices best = it.next();
+        int bestSize = best.size();
+        while (it.hasNext()) {
+            EdgeChoices n = it.next();
+            if (n.size()<bestSize) {
+                bestSize = n.size();
+                best = n;
+            }
+        }
+        return best;
+    }
+
+    private void pushChoiceFromEdge(final EdgeChoices opt) {
+        choices.remove(opt);
+        trail.push(new Undoable(){
+            @Override
+            public void undo() {
+                choices.push(opt);
+            }});
+        opt.prepareChoices(growing,shrinking);
+        if (opt.impossible()) {
+            stack.push(new Failure());
+        } if (opt.alreadyDone()) {
+            stack.push(new Frame(){
+                @Override
+                public String toString() {
+                    return "true";
+                }
+                
+                @Override
+                boolean call(int counter) {
+                    fail();
+                    return true;
+                }});
+        } else {
+            stack.push(new MakeChoiceAtFace(opt));
+        }
+    }
+
+    private void debugMsg(String port) {
+        transitions ++;
+        if (debug)
+        System.err.println(port+"["+stack.size()+"/"+trail.size()+":"+choices.size()+"] "+pad(stack)+
+                pad(trail)+pad(choices)+stack.peek().toString());
+    }
+    private String pad(Collection<?> c) {
+        int size = c.size();
+        return size<10?"  ":(size<100?" ":"");
+    }
+
+    boolean edgeRemovalFailed;
+    /**
+     * Remove an edge from the current solution space.
+     * @param t
+     * @return true if removal was ok, false to force backtracking
+     */
+    protected boolean remove(final TGEdge t) {
+        edgeRemovalFailed = false;
+        shrinking.removeEdge(t);
+        Set<TGVertex> vv = new HashSet<TGVertex>();
+        vv.add(t.source);
+        vv.add(t.dest);
+        shrinking.prune(vv,true);
+        return !edgeRemovalFailed;
+    }
+
+    void trailRemove(final TGEdge t) {
+        trail.push(new Undoable(){
+
+            @Override
+            public void undo() {
+                shrinking.addEdge(t, t.source, t.dest );
+            }});
+        for (EdgeChoices ch:this.choices) {
+            if (edgeRemovalFailed) {
+                return;
+            }
+            edgeRemovalFailed = !ch.choiceRemoved(t, this);
+        }
+    }
+
+    /**
+     * Add the edge to the current solution space.
+     * @param tension
+     * @return
+     */
+    protected boolean add(TGEdge tension) {
+        return shrinking.containsEdge(tension) && growing.addWithConsequences(tension);
+    }
+
+    private void backTrack() {
+        debugMsg("back");
+        while (trail.size() > stack.peek().trailPtr) {
+            trail.pop().undo();
+        }
+    }
+
+    private TGEdge findPossibleEdge() {
+        Iterator<TGEdge> it = 
+                Arrays.asList(shrinking.sortedEdges()).
+//                shrinking.getEdges().
+                iterator();
+        TGEdge t;
+        while (true) {
+            if (!it.hasNext()) {
+                return null;
+            }
+            t = it.next();
+            if (!growing.containsEdge(t)) {
+                return t;
+            }
+        }
+    }
+
+    public void pushUndoRemove(final GrowingGraph gg, final TGEdge t) {
+        trail.push(new Undoable(){
+            @Override
+            public void undo() {
+                TGVertex v1 = gg.getSource(t);
+                TGVertex v2 = gg.getDest(t);
+                
+                if (!gg.removeEdge(t)) {
+                    throw new IllegalStateException("Failed to remove edge");
+                }
+                if (gg.getNeighborCount(v1)==0) {
+                    gg.removeVertex(v1);
+                }
+                if (gg.getNeighborCount(v2)==0) {
+                    gg.removeVertex(v2);
+                }
+            }});
+    }
+
+    public void setDebugExpected(AbstractTGraph expected) {
+        this.expected = expected;
+        this.debug = true;
+    }
+    
+    boolean debugLookingGood() {
+        return expected == null || subGraph(this.growing, expected) && subGraph(expected, shrinking);
+    }
+
+    private boolean subGraph(AbstractTGraph small, AbstractTGraph big) {
+        for (TGEdge edge: small.getEdges()) {
+            if (small.getSource(edge) != big.getSource(edge) ) {
+                return false;
+            }
+            if (small.getDest(edge) != big.getDest(edge) ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void pushUndoReplace(final Set<TGEdge> allChoices, final TGEdge t) {
+        trail.push(new Undoable(){
+            @Override
+            public void undo() {
+                allChoices.add(t);
+            }});
+    }
+
+}
+
+
+/************************************************************************
+    This file is part of the Java Oriented Matroid Library.  
+
+    The Java Oriented Matroid Library is distributed in the hope that it 
+    will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the Java Oriented Matroid Library.  
+    If not, see <http://www.gnu.org/licenses/>.
+
+**************************************************************************/
